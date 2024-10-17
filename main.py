3f0d21593ae7447d32b3546eb459b93bd9368bf3
ryosan-7 <30883561+ryoakira12345@users.noreply.github.com>
2024-10-18 07:17:46 +0900

    Update Azure App Service build and deployment workflow config

--- main.py
+++ main.py
@@ -35,7 +35,8 @@ app.add_middleware(
         "http://localhost:3000",  # 開発時のローカルURL
         "https://office-pacrico-frontend.vercel.app",  # VercelでデプロイされたフロントエンドのURL
         "https://office-pacrico-user-frontend.vercel.app", #Vercelでデプロイされたユーザー用のフロントエンドのURL
-        "https://tech0-gen-7-step4-studentwebapp-pos-37-bxbfgkg5a7gwa7e9.eastus-01.azurewebsites.net" #AzureでデプロイされたフロントエンドのURL
+        "https://tech0-gen-7-step4-studentwebapp-pos-37-bxbfgkg5a7gwa7e9.eastus-01.azurewebsites.net" ,#Azureでデプロイされたユーザー用のフロントエンドのURL
+        "https://tech0-gen-7-step4-studentwebapp-pos-35-cubpd9h4euh3g0d8.eastus-01.azurewebsites.net/" #Azureでデプロイされたユーザー用のフロントエンドのURL
     ],
     allow_credentials=True,
     allow_methods=["*"],
