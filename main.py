b23c08430c5fbed1c257708f0dd93e784ae05634
suzuki <suzuyu33z@gmail.com>
2024-10-19 23:30:09 +0900

    no message

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ else:
 
 # データベースエンジンの作成
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, 
+    SQLALCHEMY_DATABASE_URL,
     connect_args={"check_same_thread": False} if "sqlite" in SQLALCHEMY_DATABASE_URL else {}
 )
 
