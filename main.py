2b656f7481019e10f44fc486f99c77ebdffe68c1
suzuki <suzuyu33z@gmail.com>
2024-10-19 21:11:38 +0900

    no message

--- main.py
+++ main.py
@@ -33,6 +33,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",  # 開発時のローカルURL
+        "http://127.0.0.1:3000", # 開発時のローカルURL
         "https://office-pacrico-frontend.vercel.app",  # VercelでデプロイされたフロントエンドのURL
         "https://office-pacrico-user-frontend.vercel.app", #Vercelでデプロイされたユーザー用のフロントエンドのURL
         "https://tech0-gen-7-step4-studentwebapp-pos-37-bxbfgkg5a7gwa7e9.eastus-01.azurewebsites.net" ,#Azureでデプロイされたユーザー用のフロントエンドのURL
